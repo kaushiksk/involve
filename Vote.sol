a192789d80b6fc83c2b368204d9d4d6555c9098e
kaushiksk <kaushikskalmady@gmail.com>
2018-04-10 09:02:34 +0530

    Added comments to solidity code

--- Vote.sol
+++ Vote.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.4.19;
 
 contract Vote {
-    /* Define variable owner of the type address */
+    /* Define variable owner of the type address
+       and data as an array of byte32
+       This is because web3.py does not yet allow passing strings as arguments to constructor */
     address owner;
     bytes32[] data;
-    /* This function is executed at initialization and sets the owner of the contract */
+    /* This function is executed at initialization */
     function Vote(bytes32 constituency, bytes32 candidate) public { 
             owner = msg.sender;
             data.push(constituency);
